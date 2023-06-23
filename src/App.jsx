fcc1f07432f6ffd9a606c8abfd4dee87a5108239
Leandro Furtado <le_furtado@outlook.com.br>
2023-06-22 23:36:01 -0300

    feature: (aos-scroll) "Adicionado efeito das seções ao rolar a página"

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import Banner from "./components/Banner";
 import Busca from "./components/Busca";
@@ -7,17 +8,22 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Localizacao from "./components/Localizacao";
 import Titulo from "./components/Titulo";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const BodyContainerStyle = styled.body`
   margin: 0 30px;
 
-  @media(min-width: 1280px) {
+  @media (min-width: 1280px) {
     max-width: 1228px;
     margin: auto;
   }
 `;
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <BodyContainerStyle>
       <Header />
